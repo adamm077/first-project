bc662785a68c001140316d726681c6dced5eacce
Adam Mulla <adamm076@gmail.com>
2016-06-12 13:55:30 -0700

    java practice

---
+++ new 2.java
@@ -0,0 +1,23 @@
+package //<name>
+import.java.util.Scanner;
+public class //<name>
+{
+public static void main(String args[])
+{
+	Scanner = new Scanner(System.in);
+	
+}
+}
+///////////////////////////////new
+class apples
+{
+public static void main(String args[])
+{
+	int test = 6;
+	if ( test == 9){
+		System.out.println("yes");
+	}else{System .out.println("this is else");
+	}
+		
+}	
+}
